ece81274267e0ba40d1aaf107230c5806060dd06
jlynn324 <jaclyn.yangyuen@gmail.com>
2015-04-04 16:56:08 -0700

    Basic Plain Font

--- src/Main.c
+++ src/Main.c
@@ -1,16 +1,32 @@
 #include <pebble.h>
     
 Window *g_window;
+TextLayer *g_text_layer;
 
 void window_load(Window *window)
 {
-    //We will add the creation of Window's elements
+    //We will add the creation of Window's elements, 
+    //such as, in this case, layered text
+    
+    //makes text layer and sets its frame to the full screen
+    g_text_layer = text_layer_create(GRect(0, 0, 144, 168));
+    //makes bgd color transparent
+    text_layer_set_background_color(g_text_layer, GColorClear);
+    //set text color black
+    text_layer_set_text_color(g_text_layer, GColorBlack);
+    
+    //child (root) layers drawn in front of parent (window) layers
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(g_text_layer));
+    
+    //display whatever text you want in quotes
+    text_layer_set_text(g_text_layer, "\n Emergency call\n \n \n \nCall 911 \n \n \n \nSend text message");
 }
 
 void window_unload(Window *window)
 {
-    //We will safely estroy the Window's elements here    
+    //We will safely destroy the Window's elements here    
     window_destroy(g_window);
+    text_layer_destroy(g_text_layer);
 }
 
 void init()
