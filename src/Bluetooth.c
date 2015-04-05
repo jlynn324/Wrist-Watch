4ae52c6d4947bf0b48e8b2273eefd4ab6262984e
jlynn324 <jaclyn.yangyuen@gmail.com>
2015-04-04 22:33:35 -0700

    Combined with Bluetooth, Dictionary and Button

--- src/Bluetooth.c
+++ src/Bluetooth.c
@@ -8,19 +8,50 @@
 static Window *s_main_window;
 static TextLayer *s_output_layer;
 
+//Helper function-send numbers 1-3 corresponding to the options
+//to the phone
+/*
+static void send_int(int key, int value) {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  dict_write_int(iter, key, &value, sizeof(int), true);
+    //Send the data!
+  app_message_outbox_send();
+}
+*/
 static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
     //TODO: Change to Emergency Call connection
   text_layer_set_text(s_output_layer, "Calling emergency contact");
+    
+    //Begin Bluetooth implementation
+    
+    // Prepare dictionary
+    //DictionaryIterator *iterator1;
+    //app_message_outbox_begin(&iterator1);
+    //Send signal with appropriate key and value of 1
+    //int keyy = 1;
+    //int valuee = 1;
+        
+    //send_int(keyy, valuee);
 }
 
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-    //TODO: Change to Call 911 connection
   text_layer_set_text(s_output_layer, "Calling 911");
+    //Send signal with appropriate key and value of 2
+    //int keyy = 2;
+    //int valuee = 2;
+        
+    //send_int(keyy, valuee);
 }
 
 static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
     //TODO: Change to Send Text Message connection
   text_layer_set_text(s_output_layer, "Sending message");
+    //Send signal with appropriate key and value of 3
+    //int keyy = 3;
+    //int valuee = 3;
+        
+    //send_int(keyy, valuee);
 }
 
 static void click_config_provider(void *context) {
@@ -69,3 +100,8 @@ int main(void) {
   app_event_loop();
   deinit();
 }
+    /*
+ * main.c
+ * Sets up a Window and a TextLayer, then subscribes to future
+ * Bluetooth state updates.
+ */
